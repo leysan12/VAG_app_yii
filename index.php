9585e3f1a8e8d94387916fe3888904bf8c9a3a82
leysan12 <lnu@munich-innovation.com>
2014-05-27 13:48:04 +0200

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<!--
+<!--//
 iPad minimal web app HTML/CSS template (Responsive Web Design, no JS required)
 
 @author Xavi Esteve
